dff3141d4a6e1cd5796603ecfc12b8b3e644dd32
petarjoncheski <petar.j@slicelife.com>
2019-07-10 17:13:50 +0200

    1. At TrackingViewModel.kt added actions which the fragment/activity will consume 2. At TrackingFragment.kt the actions from the TrackingViewModel.kt are observed and consumed At fragment_tracking.xml if you click on "Show prompt" a toast will be shown with the text inside the EditText. This is only to demonstrate

--- app/src/main/java/com/example/myapplication/main/home/tracking/TrackingViewModel.kt
+++ app/src/main/java/com/example/myapplication/main/home/tracking/TrackingViewModel.kt
@@ -1,13 +1,44 @@
 package com.example.myapplication.main.home.tracking
 
+import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.mangers.UserManager
 import com.example.myapplication.general.GeneralViewModel
 
-class TrackingViewModel(val userManager: UserManager) : GeneralViewModel() {
+class TrackingViewModel(
+    val userManager: UserManager
+) :
+    GeneralViewModel() {
     companion object {
-        fun createFactory(userManager: UserManager): TrackingViewModelFactory = TrackingViewModelFactory(userManager)
+        fun createFactory(userManager: UserManager): TrackingViewModelFactory =
+            TrackingViewModelFactory(userManager)
+    }
+
+    /**
+     * Define all the actions that the view should consume.
+     */
+    sealed class Actions {
+        object StartReviewOrderActivity : Actions()
+        class ShowMessage(val text: String) : Actions()
+    }
+
+    val actions = MutableLiveData<Actions>()
+    val message = ObservableField<String>("")
+
+    /**
+     * Note: You must call the setValue(T) method to update the LiveData object from the main thread.
+     * If the code is executed in a worker thread, you can use the postValue(T) method instead to update the LiveData object.
+     */
+    fun consumeReviewOrderTapped() {
+        actions.postValue(Actions.StartReviewOrderActivity)
+    }
+
+    fun consumeShowMessageTapped() {
+        message.get()?.also {
+            actions.postValue(Actions.ShowMessage(it))
+        }
     }
 }
 
