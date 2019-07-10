40f519de8749ae967a027e12b7f976b9380c44c8
petarjoncheski <petar.j@slicelife.com>
2019-07-10 17:13:50 +0200

    1. At TrackingViewModel.kt added actions which the fragment/activity will consume 2. At TrackingFragment.kt the actions from the TrackingViewModel.kt are observed and consumed At fragment_tracking.xml if you click on "Show prompt" a toast will be shown with the text inside the EditText. This is only to demonstrate

--- app/src/main/java/com/example/myapplication/main/home/tracking/TrackingFragment.kt
+++ app/src/main/java/com/example/myapplication/main/home/tracking/TrackingFragment.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication.main.home.tracking
 
+import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.myapplication.R
+import com.example.myapplication.ReviewOrderActivity
 import com.example.myapplication.mangers.UserManager
 import com.example.myapplication.databinding.FragmentTrackingBinding
 import com.example.myapplication.general.GeneralFragment
@@ -13,11 +19,37 @@ import com.example.myapplication.general.GeneralFragment
 class TrackingFragment : GeneralFragment<TrackingViewModel>() {
     private val userManager: UserManager = UserManager()
 
-    override fun bindLayout(inflater: LayoutInflater, container: ViewGroup?): View {
-        val trackingViewDataBinding: FragmentTrackingBinding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_tracking, container, false)
-        viewModel = ViewModelProviders.of(this, TrackingViewModel.createFactory(userManager))
-            .get(TrackingViewModel::class.java)
-        return trackingViewDataBinding.root
+    override fun bindLayout(inflater: LayoutInflater, container: ViewGroup?): View =
+        DataBindingUtil.inflate<FragmentTrackingBinding>(inflater, R.layout.fragment_tracking, container, false)
+            .let {
+                this.viewModel =
+                    ViewModelProviders.of(this@TrackingFragment, TrackingViewModel.createFactory(userManager))
+                        .get(TrackingViewModel::class.java)
+
+                it.viewModel = this.viewModel
+                it.root
+            }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel?.also {
+            /** IMPORTANT !!! for the observe method be sure to use [Fragment.getViewLifecycleOwner] */
+            it.actions.observe(viewLifecycleOwner, Observer<TrackingViewModel.Actions> { action ->
+                when (action) {
+                    is TrackingViewModel.Actions.StartReviewOrderActivity -> startReviewOrderActivity()
+                    is TrackingViewModel.Actions.ShowMessage -> showToast(action.text)
+                }
+            })
+        }
+    }
+
+    private fun startReviewOrderActivity() {
+        activity?.also {
+            startActivity(Intent(it, ReviewOrderActivity::class.java))
+        }
+    }
+
+    private fun showToast(text: String) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 }
